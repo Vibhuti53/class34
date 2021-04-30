68818ca27db4a3bfd52067a25afc6966c50cef90
Nikhil Vispute <Vibhuti53>
2021-04-30 18:12:32 +0530

    physics engine concepts

--- sketch.js
+++ sketch.js
@@ -6,11 +6,13 @@ const Constraint = Matter.Constraint;
 var myEngine, myWorld;
 var bg;
 var ground;
-var boggie1,boggie2,boggie3;
-var chain1;
+var boggie1,boggie2,boggie3,boggie4,boggie5,boggie6;
+var rock;
+var chain1,chain2,chain3,chain4,chain5;
 var trainSound 
 var crashSound
 var flag = 0;
+var collision
 
 function preload(){
   bg= loadImage("images/bg.jpg");
@@ -24,14 +26,59 @@ function setup() {
 
   ground = new Ground(600,380,1200,20);
 
+  boggie1 = new Boggie(50,170,50,50);
+  boggie2 = new Boggie(150,170,50,50);
+  boggie3 = new Boggie(250,170,50,50);
+  boggie4 = new Boggie(350,170,50,50);
+  boggie5 = new Boggie(450,170,50,50);
+  boggie6 = new Boggie(550,170,50,50);
 
+  rock = new Rock(1100,200,100,100);
+
+  chain1 = new Chain(boggie1.body, boggie2.body);
+  chain2 = new Chain(boggie2.body, boggie3.body);
+  chain3 = new Chain(boggie3.body, boggie4.body);
+  chain4 = new Chain(boggie4.body, boggie5.body);
+  chain5 = new Chain(boggie5.body, boggie6.body);
 }
 
 function draw() {
   background(bg);  
   Engine.update(myEngine);
 
- 
+  boggie1.show();
+  boggie2.show();
+  boggie3.show();
+  boggie4.show();
+  boggie5.show();
+  boggie6.show();
+  rock.show();
+  chain1.show();
+  chain2.show();
+  chain3.show();
+  chain4.show();
+  chain5.show();
+
+  collision = Matter.SAT.collides(boggie6.body, rock.body);
+
+  if(collision.collided){
+    flag = 1;
   }
 
+  if(flag === 1){
+    textSize(34);
+    stroke(2);
+    fill("blue");
+    text("Crash",600,150);
+
+    crashSound.play();
+  }
+  
+}
+function keyPressed(){
+  if(keyCode === RIGHT_ARROW){
+    Matter.Body.applyForce(boggie6.body, {x: boggie6.body.position.x, y: boggie6.body.position.y},{x: 0.5, y:0});
+    trainSound.play();
+  }
+}
   
